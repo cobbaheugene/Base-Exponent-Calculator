0f112a5ee51ee2c342da56507bc185bb6ca85377
Eugene Cobbah <cobbaheugene@gmail.com>
2021-08-13 17:10:42 +0000

    Base-Exponent Calculator

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int variableB;
+	float variableA, solution;
+	int i; //i is for the position. an incremental value
+	int power = 1;
+
+	cout << "Enter value of the base: ";
+	cin >> variableA;
+	cout << "Entre value of the exponent: ";
+	cin >> variableB;
+
+	//we use the for loop here to derive the answer for the
+	//function of the integer and real number, since the steps of the computation will be repeated
+	for (i = 1; i <= variableB; i++) {
+		power *= variableA; //
+	}
+
+	solution = power;
+	cout << "Your answer is = " << solution << endl;
+	system("pause>0");
+	return 0;
+}
